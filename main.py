414db561778392c998201e88f58140b273bec46f
TakeKnowledge <starmekey@naver.com>
2021-06-05 23:45:18 +0900

    set naver site

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+driver = webdriver.Chrome()
+driver.get("http://naver.com/")
 
+login_button = driver.find_element_by_css_selector(".link_login")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+login_button.click()
