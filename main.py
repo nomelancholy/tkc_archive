027b2380b4fcfbc496c76a0d063bad7d0442c8e6
TakeKnowledge <starmekey@naver.com>
2021-06-05 23:51:34 +0900

    add all sites

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ driver.get("http://naver.com/")
 
 login_button = driver.find_element_by_css_selector(".link_login")
 
+driver.get("https://dctribe.com/")
+
+driver.get("https://hiphople.com/")
+
+driver.get("http://www.todayhumor.co.kr/")
+
 login_button.click()
