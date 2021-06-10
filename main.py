9734ae282a9fda594c7752d1f5822485a86f0c7d
nomelancholy <starmekey@naver.com>
2021-06-10 10:56:56 +0900

    install pyperclip and avoid naver capcha

--- main.py
+++ main.py
@@ -1,45 +1,80 @@
 import os
+import time
+import pyperclip
+
 from selenium import webdriver
 from dotenv import load_dotenv,find_dotenv
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import  Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.get("http://naver.com/")
-load_dotenv(find_dotenv())
 
-NAVER_ID = os.environ.get("NAVER_ID")
+def naver_process():
+    driver.get("http://naver.com/")
+    load_dotenv(find_dotenv())
 
-try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'link_login'))
-    )
-except:
-    driver.quit()
+    NAVER_ID = os.environ.get("NAVER_ID")
+    NAVER_PW = os.environ.get("NAVER_PW")
 
-login_button = driver.find_element_by_css_selector(".link_login")
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'link_login'))
+        )
+    except:
+        driver.quit()
 
-login_button.click()
+    login_button = driver.find_element_by_css_selector(".link_login")
 
-try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, 'id_area'))
-    )
-except:
-    driver.quit()
+    login_button.click()
 
-input_field = driver.find_element_by_id('id')
-pw_field = driver.find_element_by_id('pw_area')
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, 'id_area'))
+        )
+    except:
+        driver.quit()
 
-input_field.click()
-input_field.send_keys(NAVER_ID)
+    id_field = driver.find_element_by_id('id')
+    pw_field = driver.find_element_by_id('pw')
+
+    id_field.click()
+    pyperclip.copy(NAVER_ID)
+    id_field.send_keys(Keys.CONTROL, 'v')
+    time.sleep(1)
+
+    pw_field.click()
+    pyperclip.copy(NAVER_PW)
+    pw_field.send_keys(Keys.CONTROL, 'v')
+    time.sleep(2)
+
+    submit_button = driver.find_element_by_id("log.login")
+    submit_button.click()
+
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="NM_FAVORITE"]/div[1]/ul[1]/li[3]/a'))
+        )
+    except:
+        driver.quit()
+
+    driver.get('https://blog.naver.com/starmekey')
 
 
-# driver.get("https://dctribe.com/")
-#
-# driver.get("https://hiphople.com/")
-#
-# driver.get("http://www.todayhumor.co.kr/")
+def dct_process():
+    driver.get("https://dctribe.com/")
+
+def hiphople_process():
+    driver.get("https://hiphople.com/")
+
+def o_u_process():
+    driver.get("http://www.todayhumor.co.kr/")
+
+naver_process()
+# dct_process()
+# hiphople_process()
+# o_u_process()
+
 
